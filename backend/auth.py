ec98a8f9866b82d0ca924e2cc868475b5402e4f7
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-09-07 07:41:52 +0530

    feat: implement lgoin and registeration

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,6 @@
-import functools
-from typing import Any
-
 from flask import Blueprint, flash, g, render_template, request, session, url_for
 from flask.helpers import redirect
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from backend.db import get_db
 
@@ -61,19 +58,115 @@ def admin():
             session["user_id"] = user[0]
             return redirect(url_for("index"))
 
-        flash(error)
+        flash(error, category="error")
 
     return render_template("auth/admin.html")
 
 
-@bp.route("/register")
+@bp.route("/register", methods=["GET", "POST"])
 def register():
-    return "blank"
+    db = get_db()
+    states = []
+    cities = []
+    error = []
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        family_name = request.form["family_name"]
+        annual_income = request.form["annual_income"]
+        property_count = request.form["property_count"]
+        state = request.form["state"]
+        district = request.form["district"]
+        pincode = request.form["pincode"]
+
+        with db.cursor() as cursor:
+            address = cursor.execute(
+                "SELECT address_id FROM address where district_id = :districtid",
+                (district,),
+            ).fetchone()
+
+            if address is None:
+                error.append({"id": "pincode"})
+            else:
+                distributor = cursor.execute(
+                    "SELECT distributor_id FROM distributor WHERE address_id = :addressid",
+                    (address[0],),
+                ).fetchone()
+
+                cursor.execute(
+                    "INSERT INTO family(family_name, property, address_id, distributor_id) VALUES(:familyname, :property, :addressid,  :distributorid)",
+                    (
+                        family_name,
+                        property_count,
+                        address[0],
+                        distributor[0],
+                    ),
+                )
+
+                family_id = cursor.execute(
+                    "SELECT family_id FROM family WHERE family_name = :familyname AND property = :property AND address_id = :addressid AND distributor_id = :distributor_id",
+                    (
+                        family_name,
+                        property_count,
+                        address[0],
+                        distributor[0],
+                    ),
+                ).fetchone()[0]
+
+                cursor.execute(
+                    "INSERT INTO family_user(family_id, username, password) VALUES (:familyid, :username, :password)",
+                    (
+                        family_id,
+                        username,
+                        generate_password_hash(password),
+                    ),
+                )
+                db.commit()
+                return redirect(url_for("auth.login"))
+
+    with db.cursor() as cursor:
+        states = cursor.execute(
+            "SELECT * FROM state ORDER BY state_name ASC"
+        ).fetchall()
+        cities = cursor.execute(
+            "SELECT * FROM district ORDER BY district_name ASC"
+        ).fetchall()
+
+    return render_template(
+        "auth/register.html", states=states, cities=cities, error=error
+    )
 
 
-@bp.route("/login")
+@bp.route("/login", methods=["GET", "POST"])
 def login():
-    return "blank"
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db = get_db()
+        user = None
+        error: str | None = None
+
+        with db.cursor() as cursor:
+            user = cursor.execute(
+                "SELECT * FROM family_user WHERE username = :username", (username,)
+            ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user[2], password):
+            error = "Incorrect password."
+
+        if error is None and user is not None:
+            session.clear()
+            session["family_id"] = user[1]
+            return redirect(url_for("index"))
+        elif error is not None:
+            flash(error, category="error")
+    return render_template("auth/login.html")
 
 
 @bp.route("/logout")
