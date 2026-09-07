c6810873eb3b363329bb1868dd4fccfc14ed38a0
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-09-07 07:42:43 +0530

    chore: import bootstrap

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,12 +2,16 @@ import os
 
 from flask import Flask, render_template
 
+from flask_bootstrap import Bootstrap5
+
 from . import auth, db
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY="dev", DATABASE="hello")
+
+    bootstrap = Bootstrap5(app)
 
     db.init_app(app)
 
